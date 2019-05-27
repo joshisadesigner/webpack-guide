786d91122b9c17fa68820e1cbc5d084cc3edc26c
joshisadesigner <joshisadesigner@gmail.com>
2019-05-27 16:00:57 -0600

    Change indentation tab width

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,54 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-
-                    {
-                        loader: 'style-loader',
-                        /**
-                         * Add css loaders otherwhise wont work
-                         */
-                        /**
-                         * When style-loader is used with 
-                         * { options: { sourceMap: true } } option,
-                         * the CSS modules will be generated as Blobs
-                         */
-                        options: { sourceMap: true }
-                    },
-                    {
-                        loader: 'css-loader',
-                        /**
-                         * When style-loader is used with 
-                         * { options: { sourceMap: true } } option,
-                         * the CSS modules will be generated as Blobs
-                         */
-                        options: { sourceMap: true }
-                    },
-                ]
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            /**
+             * Add css loaders otherwhise wont work
+             */
+            /**
+             * When style-loader is used with
+             * { options: { sourceMap: true } } option,
+             * the CSS modules will be generated as Blobs
+             */
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'css-loader',
+            /**
+             * When style-loader is used with
+             * { options: { sourceMap: true } } option,
+             * the CSS modules will be generated as Blobs
+             */
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            /**
+             * Return a Data URL if the file is smaller than byte limit
+             * Fallback default 'file-loader'
+             */
+            loader: 'url-loader',
+            options: {
+              limit: 50000,
             },
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use: [
-                    {
-                        /**
-                         * Return a Data URL if the file is smaller than byte limit
-                         * Fallback default 'file-loader'
-                         */
-                        loader: 'url-loader',
-                        options: {
-                            limit: 50000
-                        }
-                    }
-                ]
-            },
-        ]
-    }
-}
+          },
+        ],
+      },
+    ],
+  },
+};
